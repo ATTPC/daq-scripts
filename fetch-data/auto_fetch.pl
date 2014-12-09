cfcab247f43c7eb1d65e02820438e90dc228710f
attpc <attpc_nscl@gmail.com>
2014-12-09 09:01:37 -0500

    Fixed task naming in fetch.yml. Added auto_fetch.pl to automate the task of fetching and rearranging data (and to make the syntax cleaner).

---
+++ fetch-data/auto_fetch.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+if (@ARGV == 0) {
+	print "Please provide an experiment name as an argument.\n" and exit 1;
+}
+
+my $expname = $ARGV[0];
+
+print "Fetching and rearranging data for experiment $expname.\n";
+
+my @ansible_cmd = ("ansible-playbook","/home/attpc/ansible-attpc/fetch-data/fetch.yml",'-e exp_name\=$expname');
+my @rearrange_cmd = ("/home/attpc/ansible-attpc/rearrange_data.pl","$expname");
+
+system(@ansible_cmd);
+system(@rearrange_cmd);
