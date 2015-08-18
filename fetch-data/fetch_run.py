4752376743013841f1db288e4596b854c1bc8e9e
Joshua Bradt <bradt@nscl.msu.edu>
2015-08-18 09:55:14 -0400

    Made fetch-run work for arbitrary local paths.

--- fetch-data/fetch_run.py
+++ fetch-data/fetch_run.py
@@ -10,11 +10,11 @@ file.
 
 After running this command, the files will be found in
 
-    /data/[exp_name]/[run_name]
+    [local_root]/[exp_name]/[run_name]
 
 Usage:
 
-    ./fetch_run.py <exp_name> <run_name>
+    ./fetch_run.py <exp_name> <run_name> <local_root>
 
 For the experiment name, provide the name of an experiment known to Narval.
 For the run name, give a string like 'run_0021' or 'run_0142'.
@@ -28,18 +28,18 @@ import glob
 import subprocess
 import shutil
 
-def fetch(exp_name, run_name):
+def fetch(exp_name, run_name, local_root):
     playbook_path = os.path.expanduser('~/daq-scripts/fetch-data/fetch-file.yml')
-    vars_str = 'exp_name={e} run_name={r}'.format(e=exp_name, r=run_name)
+    vars_str = 'exp_name={e} run_name={r} local_root={l}'.format(e=exp_name, r=run_name, l=local_root)
     print('Fetching data files:')
     command = ['ansible-playbook', playbook_path, '--extra-vars', vars_str]
     ret = subprocess.call(command)
     if ret != 0:
         raise RuntimeError('Ansible failed to fetch data: code {}'.format(ret))
 
-def rearrange_files(exp_name, run_name):
-    fetch_dir = os.path.join('/data', exp_name, 'temp')  # where the files are
-    clean_dir = os.path.join('/data', exp_name)
+def rearrange_files(exp_name, run_name, local_root):
+    fetch_dir = os.path.join(local_root, exp_name, 'temp')  # where the files are
+    clean_dir = os.path.join(local_root, exp_name)
     run_dir = os.path.join(clean_dir, run_name)  # where the files will go
 
     # Find files for this run
@@ -68,13 +68,14 @@ def rearrange_files(exp_name, run_name):
         shutil.move(fp, new_path)
 
 def main():
-    if len(sys.argv) < 3:
-        sys.exit('Provide experiment name and run name')
+    if len(sys.argv) < 4:
+        sys.exit('Provide experiment name, run name, and local data root')
     exp_name = sys.argv[1]
     run_name = sys.argv[2]
+    local_root = sys.argv[3]
 
-    fetch(exp_name, run_name)
-    rearrange_files(exp_name, run_name)
+    fetch(exp_name, run_name, local_root)
+    rearrange_files(exp_name, run_name, local_root)
 
 if __name__ == '__main__':
     main()
